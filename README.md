33d4cfa21b28328cd1ced47100d5161255d349c2
snow1313113 <59796717@qq.com>
2021-07-04 20:30:25 +0800

    gen homework for child

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # holiday_homework
+
+### build
+g++ main.cpp -std=c++17 -Wall -o homework
+
+### use 
+./homework
+./homework min_num max_num
+./homework min_num max_num num_per_page
+./homework min_num max_num num_per_page total
---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <random>
+
+size_t random_one(size_t a, size_t b)
+{
+    if (a >= b)
+        return a;
+
+    static std::random_device g_random_device;
+    static std::mt19937 mt(g_random_device());
+    std::uniform_int_distribution<size_t> gen(a, b);
+    return gen(mt);
+}
+
+int main(int argc, char *argv[])
+{
+    size_t min_num = 1;
+    size_t max_num = 10;
+    size_t total = 200;
+    size_t num_per_page = 10;
+
+    if (argc > 5 || argc == 2)
+    {
+        std::cout << "usage : ./homework min_num max_num num_per_page total" << std::endl;
+        return 0;
+    }
+
+    if (argc >= 3)
+    {
+        min_num = std::strtoul(argv[1], nullptr, 10);
+        max_num = std::strtoul(argv[2], nullptr, 10);
+        if (argc >= 4)
+        {
+            num_per_page = std::strtoul(argv[3], nullptr, 10);
+            if (argc == 5)
+                total = std::strtoul(argv[4], nullptr, 10);
+        }
+    }
+
+    std::cout << "---\nmarp: true\ntheme: gaia\n_class: invert\n";
+
+    for (size_t i = 0; i < total; ++i)
+    {
+        if (i % num_per_page == 0)
+            std::cout << "---\n";
+        std::cout << "- " << random_one(min_num, max_num) << " + " << random_one(min_num, max_num) << " =\n";
+    }
+
+    return 0;
+}
