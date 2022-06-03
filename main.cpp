0daa8857f4381fa364199496f918fa7642183cd1
snow1313113 <59796717@qq.com>
2022-06-03 19:19:15 +0800

    add subtraction

--- main.cpp
+++ main.cpp
@@ -14,6 +14,42 @@ size_t random_one(size_t a, size_t b)
     return gen(mt);
 }
 
+enum OP_TYPE
+{
+    OP_ADD = 0,
+    OP_SUB = 1,
+};
+
+void gen_add(size_t min_num_, size_t max_num_)
+{
+    std::cout << "- " << random_one(min_num_, max_num_) << " + " << random_one(min_num_, max_num_) << " =\n";
+}
+
+void gen_sub(size_t min_num_, size_t max_num_)
+{
+    size_t first = random_one(min_num_, max_num_);
+    std::cout << "- " << first << " - " << random_one(min_num_, first) << " =\n";
+}
+
+void gen_question(OP_TYPE type_, size_t min_num_, size_t max_num_)
+{
+    switch (type_)
+    {
+    case OP_ADD:
+        {
+            gen_add(min_num_, max_num_);
+            break;
+        }
+    case OP_SUB:
+        {
+            gen_sub(min_num_, max_num_);
+            break;
+        }
+    default:
+        break;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     // default params
@@ -42,11 +78,15 @@ int main(int argc, char *argv[])
 
     std::cout << "---\nmarp: true\nsize: 4:3\npaginate: true\ntheme: default\n";
 
+    std::vector<OP_TYPE> all_types;
+    all_types.push_back(OP_ADD);
+    all_types.push_back(OP_SUB);
     for (size_t i = 0; i < total; ++i)
     {
         if (i % question_per_page == 0)
             std::cout << "---\n";
-        std::cout << "- " << random_one(min_num, max_num) << " + " << random_one(min_num, max_num) << " =\n";
+        auto index = random_one(0, all_types.size());
+        gen_question(all_types[index], min_num, max_num);
     }
 
     return 0;
